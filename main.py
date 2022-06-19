6694827341e776879ddc483d79bf7cb3b85d522f
unknown <herambpatil2004@gmail.com>
2022-06-19 14:56:05 +0530

    initial Commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# Importing OS Module to edit file names
+import os
+
+
+def main():
+    folder = "IMAGES"
+    # os.listdir(folder) to list directory files
+    for count, filename in enumerate(os.listdir(folder)):
+        # Edit the contents of f string to if you want to remove or
+        # take a specific parts of strings, string manipulations can be used
+
+        destination = f"-20220610-SUR-{filename[5:]}"
+        print(destination) # This prints edited file names
+        src = f"{folder}/{filename}"  # folder name/filename, if .py file is outside folder
+        destination = f"{folder}/{destination}"
+
+        # rename() function will
+        # rename all the files
+        os.rename(src, destination)
+
+
+if __name__ == '__main__':
+    # Calling main() function
+    main()
