2771e4ff14f4951827f7b5e7be459c36fae6f59b
unknown <herambpatil2004@gmail.com>
2022-07-10 22:48:39 +0530

    m

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ import os
 def main():
     folder = "IMAGES"
     # os.listdir(folder) to list directory files
+    imagemani = input("Enter the prefix (NO SPACES IF YOU WANT TO USE AS LINK: ")
     for count, filename in enumerate(os.listdir(folder)):
         # Edit the contents of f string to if you want to remove or
         # take a specific parts of strings, string manipulations can be used
 
-        destination = f"-20220610-SUR-{filename[5:]}"
+
+        destination = f"{imagemani}{filename[5:]}"
         print(destination) # This prints edited file names
         src = f"{folder}/{filename}"  # folder name/filename, if .py file is outside folder
         destination = f"{folder}/{destination}"
